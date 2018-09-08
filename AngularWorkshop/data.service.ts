c1af6defbab780ad0eed4bb83ed98258eaa1dc76
Bhavik Barot <bhavik.barot@nagarro.com>
2018-09-08 14:47:25 -0700

    Added notes to understand the difference between BehaviorSubject and Subject.

--- AngularWorkshop/data.service.ts
+++ AngularWorkshop/data.service.ts
@@ -4,6 +4,11 @@ import { BehaviorSubject } from 'rxjs'
 @Injectable()
 export class DataService {
 
+    // Notes: Difference between BehavirorSubject and Subject
+    //        BehaviourSubject will return the initial value or the current value on Subscription
+    //        Subject doesnot return the current value on Subscription. It triggers only on .next(value) call and return/output the value
+    //        Link: https://stackoverflow.com/questions/43348463/what-is-the-difference-between-subject-and-behaviorsubject/43351340
+
     private messageSource = new BehaviorSubject("This is default message from Service");
 
     curretMessage = this.messageSource.asObservable();
