4d3df974a2aa0b9236a5c47e50ba3eaf381f5a02
Bhavik Barot <bhavik.barot@nagarro.com>
2018-09-16 10:53:50 -0700

    Modified the example to use HostBinding along with HostListener.

--- AngularWorkshop/ClientApp/app/HoverOverDirective.ts
+++ AngularWorkshop/ClientApp/app/HoverOverDirective.ts
@@ -1,4 +1,4 @@
-﻿import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+﻿import { Directive, ElementRef, HostListener, Renderer2, HostBinding } from '@angular/core';
 
 @Directive(
     {
@@ -12,13 +12,20 @@ export class HoverOverDirective {
 
     }
 
+    @HostBinding('style.color') foreGroundColor: string = 'black';
+
     @HostListener('mouseover') onMouseOver() {
-        this.renderer.setStyle(this.el.nativeElement, 'color', 'Red');
+      //  this.renderer.setStyle(this.el.nativeElement, 'color', 'Red');
             //let part = this.el.nativeElement.querySelector('');
+
+        // Note: using HostBinding instead of manipulating the NativeElement.
+        this.foreGroundColor = 'Red';
     }
 
     @HostListener('mouseout') onMouseOut() {
-        this.renderer.setStyle(this.el.nativeElement, 'color', 'Black');
+       // this.renderer.setStyle(this.el.nativeElement, 'color', 'Black');
         //let part = this.el.nativeElement.querySelector('');
+
+        this.foreGroundColor = 'black';
     }
 }
